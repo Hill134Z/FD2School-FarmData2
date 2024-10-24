2a2bd99825be6a2d9f0149cd6d2af4cb8bc3d39b
Hill134Z <zhongyu@dickinson@edu>
2024-10-24 19:43:49 -0400

    Added check farm name, user name, and language

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.correct_generation.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.correct_generation.spec.js
@@ -10,4 +10,13 @@ describe("Test the generate report button", () => {
     cy.get("[data-cy=title-text]")
       .should("be.visible")
   })
+  it("Check the farm name, user name, and language", () => {
+    cy.get("[data-cy=generate-report-btn]").click()
+    cy.get("[data-cy=farm-name]")
+      .should("have.text","Farm:Sample Farm")
+    cy.get("[data-cy=user-name]")
+      .should("contain.text","manager")
+    cy.get("[data-cy=language]")
+      .should("have.text","English")
+  })
 })
